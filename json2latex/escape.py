f3a0cf1da6a1cef222adea016f7dc15bc275ce37
Dan Wysocki <dwysocki@users.noreply.github.com>
2020-12-02 22:02:43 -0600

    Optimizing escape function
    Using Python's builtin `str.translate` to optimize/streamline the `escape` function.

--- json2latex/escape.py
+++ json2latex/escape.py
@@ -1,3 +1,22 @@
+_replacements = {
+    "&": r"\&",
+    "%": r"\%",
+    "$": r"\$",
+    "#": r"\#",
+    "_": r"\_",
+    "{": r"\{",
+    "}": r"\}",
+    "~": r"\textasciitilde{}",
+    "^": r"\^{}",
+    "\\": r"\textbackslash{}",
+    "\n": "\\newline%\n",
+    "-": r"{-}",
+    "\xA0": "~",  # Non-breaking space
+    "[": r"{[}",
+    "]": r"{]}",
+}
+_replacement_lookup = str.maketrans(_replacements)
+
 def escape(string):
     """
     Replace special characters with their equivalent LaTeX macros.
@@ -8,24 +27,4 @@ def escape(string):
     Returns:
         str: The string with special characters replaced with macros.
     """
-    replacements = {
-        "&": r"\&",
-        "%": r"\%",
-        "$": r"\$",
-        "#": r"\#",
-        "_": r"\_",
-        "{": r"\{",
-        "}": r"\}",
-        "~": r"\textasciitilde{}",
-        "^": r"\^{}",
-        "\\": r"\textbackslash{}",
-        "\n": "\\newline%\n",
-        "-": r"{-}",
-        "\xA0": "~",  # Non-breaking space
-        "[": r"{[}",
-        "]": r"{]}",
-    }
-    escaped = ""
-    for char in string:
-        escaped += replacements.get(char, char)
-    return escaped
+    return string.translate(_replacement_lookup)
